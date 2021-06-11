f9de5f499cddfa0784f7192365582feb5d1ddb09
Michael Bestas <mkbestas@gmail.com>
2021-06-11 20:52:07 +0300

    Relocate privileged extension to product partition
    Change-Id: Ib798316ac47028f4f9731ddb5b6eb78bb2c859f9
    (cherry picked from commit 40918bea24e39ed23f4de461351f5c94e037acfe)

--- app/src/main/Android.mk
+++ app/src/main/Android.mk
@@ -4,8 +4,9 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := permissions_org.fdroid.fdroid.privileged.xml
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/permissions
+LOCAL_MODULE_PATH := $(TARGET_OUT_PRODUCT_ETC)/permissions
 LOCAL_SRC_FILES := $(LOCAL_MODULE)
+LOCAL_PRODUCT_MODULE := true
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
@@ -19,4 +20,5 @@ LOCAL_SRC_FILES := $(call all-java-files-under, java) \
                    $(call all-Iaidl-files-under, aidl)
 LOCAL_AIDL_INCLUDES := $(LOCAL_PATH)/aidl
 LOCAL_REQUIRED_MODULES := permissions_org.fdroid.fdroid.privileged.xml
+LOCAL_PRODUCT_MODULE := true
 include $(BUILD_PACKAGE)
